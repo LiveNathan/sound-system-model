248c5e30b9f1be849cccb082de1f987158645f41
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-17 21:27:04 -0500

    Add WebGL compatibility and axes labels, fix canvas sizing
    Introduced WebGL compatibility checks to ensure app runs only on supported devices. The camera aspect ratio and renderer size now adjust dynamically based on the window size. Axes helpers were also added to the 3D scene for better orientation and understanding of the 3D space. Labels 'X', 'Y', and 'Z' are allotted to the axes. Changes made to utilize CSS3DRenderer for displaying text labels in the 3D space.

--- src/main.js
+++ src/main.js
@@ -1,24 +1,76 @@
 import './index.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+const container = document.getElementById("container");
+const camera = new THREE.PerspectiveCamera( 75, container.offsetWidth / container.offsetHeight, 0.1, 1000 );
+const canvas = document.querySelector('#canvas');
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(container.offsetWidth, container.offsetHeight);
 const controls = new OrbitControls( camera, renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// scene.add( cube );
 
 camera.position.z = 5;
+
+const axesHelper = new THREE.AxesHelper( 5 );
+scene.add( axesHelper );
+
+
+var labelRenderer = new CSS3DRenderer();
+labelRenderer.setSize(container.offsetWidth, container.offsetHeight);
+container.appendChild(renderer.domElement);
+
+function makeTextSprite(message) {
+    var element = document.createElement('div');
+    element.style.width = '100px';
+    element.style.height = '100px';
+    element.style.backgroundColor = 'rgba(0,0,0,0)';
+    element.style.color = 'red';
+    element.innerHTML = message;
+    return element;
+}
+
+var labels = ['X', 'Y', 'Z'];
+
+for (var i = 0; i < 3; ++i) {
+    var sprite = makeTextSprite(labels[i]);
+    var sprite3D = new CSS3DObject(sprite);
+    sprite3D.position.set((i==0) ? 6 : 0, (i==1) ? 6 : 0, (i==2) ? 6 : 0); // position labels
+    scene.add(sprite3D);
+}
+
+
+
 
 function animate() {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
+    labelRenderer.render(scene, camera);
 }
-animate();
+
+
+if ( WebGL.isWebGLAvailable() ) {
+
+    animate();
+
+} else {
+
+    const warning = WebGL.getWebGLErrorMessage();
+    document.getElementById( 'container' ).appendChild( warning );
+
+}
+
+window.addEventListener( 'resize', onWindowResize, false );
+
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
