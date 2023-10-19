3cbd472b1a5f9678e362d34d8fe25aa58a14b21c
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-10-19 13:01:18 -0500

    Refactor and simplify main.js for improved readability
    This commit does two major things:
    1. Refactored the main.js file to enhance the readability and maintainability of the codebase.
    2. Simplified the position assignments and element creations, reducing redundancy and improving the fluency of the code.
    
    Unused variables were also removed - positiveAxisLength and labelOffset. Considering future changes and maintenance, this change will make the codebase more readable, easy to interpret.
    
    In main.js, the camera's position is modified and scene's objects positions are adjusted accordingly. Adjustments were made to improve the visualization of the scene. Detailed additions related to objects in the scene as part of the 3D module have also been done.
    
    These changes will make the logic of the script clearer for future collaborators.

--- src/main.js
+++ src/main.js
@@ -1,62 +1,57 @@
 import './index.css'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import WebGL from 'three/addons/capabilities/WebGL.js';
-import { CSS3DRenderer, CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
+import {CSS3DRenderer, CSS3DObject} from 'three/addons/renderers/CSS3DRenderer.js';
 import {CSS2DRenderer, CSS2DObject} from 'three/addons/renderers/CSS2DRenderer.js';
 
 const scene = new THREE.Scene();
 const container = document.getElementById("container");
-const camera = new THREE.PerspectiveCamera( 75, container.offsetWidth / container.offsetHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, container.offsetWidth / container.offsetHeight, 0.1, 1000);
 const canvas = document.querySelector('#canvas');
 const renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize(container.offsetWidth, container.offsetHeight);
 container.appendChild(renderer.domElement);
 
-// camera.position.z = 5;
 camera.up.set(0, 0, 1);
-camera.position.set(40, -20, 20);
+camera.position.set(20, -10, 15);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.object.up.set(0, 0, 1);
 controls.update();
 
-const axesHelper = new THREE.AxesHelper( 5 );
-scene.add( axesHelper );
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
 
 const labelRenderer = new CSS2DRenderer();
 labelRenderer.setSize(container.offsetWidth, container.offsetHeight);
 labelRenderer.domElement.style.position = 'absolute';
 labelRenderer.domElement.style.pointerEvents = 'none';
-// labelRenderer.domElement.style.top = '0px';
 container.appendChild(labelRenderer.domElement);
 
-const positiveAxisLength = 5;  // Ending point of axes from center
-const labelOffsets = [0.5, 0.5, 0];  // To fine-tune the exact position of labels if needed
-
-// Create HTML elements for each label
 let xLabelDiv = document.createElement('div');
 xLabelDiv.className = 'text-red-600 text-base p-1 m-1';
 xLabelDiv.textContent = 'X';
-
 let yLabelDiv = document.createElement('div');
+
 yLabelDiv.className = 'text-axes-green text-base p-1 m-1';
 yLabelDiv.textContent = 'Y';
-
 let zLabelDiv = document.createElement('div');
+
 zLabelDiv.className = 'text-blue-600 text-base p-1 m-1';
 zLabelDiv.textContent = 'Z';
-
 // Create CSS2DObject for each label
+
 let xLabel = new CSS2DObject(xLabelDiv);
 let yLabel = new CSS2DObject(yLabelDiv);
 let zLabel = new CSS2DObject(zLabelDiv);
-
 // Position labels at ends of the axes
-xLabel.position.set(0.6, 0, 0);
-yLabel.position.set(0, 0.6, 0);
-zLabel.position.set(0, 0, 0.6);
+
+const labelOffset = 0.6;
+xLabel.position.set(labelOffset, 0, 0);
+yLabel.position.set(0, labelOffset, 0);
+zLabel.position.set(0, 0, labelOffset);
 
 // Add labels to the scene
 scene.add(xLabel);
@@ -64,43 +59,58 @@ scene.add(yLabel);
 scene.add(zLabel);
 
 
-
-
-
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const sphere = new THREE.Mesh( geometry, material );
-sphere.position.set(0, 20, 10);
-scene.add( sphere );
+const material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const main = new THREE.Mesh(geometry, material);
+const edges = new THREE.EdgesGeometry(geometry);
+const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000 }));
+main.add(line);
+let mainY = 20;
+main.position.set(0, mainY, 10);
+scene.add(main);
 
+const mirroredMainMaterial = new THREE.MeshBasicMaterial({color: 0xff0000});
+const mirroredMain = new THREE.Mesh(geometry, mirroredMainMaterial);
+const edgesMirroredMain = new THREE.EdgesGeometry(geometry);
+const lineMirroredMain = new THREE.LineSegments(edgesMirroredMain, new THREE.LineBasicMaterial({ color: 0x000000 }));
+mirroredMain.add(lineMirroredMain);
+mirroredMain.position.set(0, -mainY, 10);
+scene.add(mirroredMain);
 
-
-
-
+let subLocationX = 0;
+const subDimensions = {depth: 1, width: 1, height: 1};
+const subGeometry = new THREE.BoxGeometry(subDimensions.width, subDimensions.height, subDimensions.depth);
+const subMaterial = new THREE.MeshBasicMaterial({color: 0x0000ff});
+const sub = new THREE.Mesh(subGeometry, subMaterial);
+const edgesSub = new THREE.EdgesGeometry(geometry);
+const lineSub = new THREE.LineSegments(edgesSub, new THREE.LineBasicMaterial({ color: 0x000000 }));
+sub.add(lineSub);
+sub.position.set(-subDimensions.depth/2, 0, subDimensions.height/2);
+scene.add(sub);
 
 
 function animate() {
-    requestAnimationFrame( animate );
-    renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
     labelRenderer.render(scene, camera);
 }
 
 
-if ( WebGL.isWebGLAvailable() ) {
+if (WebGL.isWebGLAvailable()) {
 
     animate();
 
 } else {
 
     const warning = WebGL.getWebGLErrorMessage();
-    document.getElementById( 'container' ).appendChild( warning );
+    document.getElementById('container').appendChild(warning);
 
 }
 
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false);
 
-function onWindowResize(){
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
